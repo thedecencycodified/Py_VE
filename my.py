f0a5a59750139d24b428d7212cca27aad584506d
thedecencycodified <farhanqadir1000#hotmail.com>
2022-09-16 23:50:55 +0500

    my 2nd commit

---
+++ my.py
@@ -0,0 +1,4 @@
+Name = str(input("Enter your Name: "))
+percentage = int(input("Enter Your Percentage: "))
+if percentage >= 70:
+    print('{} you got {}% and placed in 1st Division'.format(Name, percentage))
