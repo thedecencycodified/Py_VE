4ea5983609f9bb489461a11f0ff539e8e0507130
thedecencycodified <farhanqadir1000@hotmail.com>
2022-09-17 02:06:15 +0500

    3.1 Basics of Python

---
+++ 3.1.  Basics of Python.py
@@ -0,0 +1,49 @@
+# ===========Comments================
+# Comments are ignored by python interpreter and used by the programmer or reader to understand or remind the program
+
+# a = 666
+# b = 555
+#
+#
+# def func():
+#     print(a+b)
+#
+#
+# func()
+# here in this print function i am adding a and b together
+
+# ============Spacing================
+# Spacing # python is space sensitve # indentation error
+
+# ============Syntax============
+# Syntax
+# ab = 34
+# c = 44
+# print(ab/c)
+
+# ========Constant & Variable==========
+# Constant
+# Variable
+# 4x + 5y = 77 # 4x # 4 cannot be changed because its fixed and known as constant, it cannot store any value
+# x can be changed, it can store any type of value
+
+# var    const
+# number = 77, 88, 99
+
+# =============Data Types==========
+# Data types in python
+# string = str # alphanumeric/alphabets/special character/numbers as text/text # examples 'ali' "ali123" '''ali@123'''
+# integer = int # numbers/numeric values # examples 12 555 1000
+# float = float # number with decimal points # examples 1.1 9.5 99.99
+
+# ===========Input Fuction===========
+my_input = input('input your data: ')  # str
+my_input2 = str(input('input your string data: '))  # string
+my_input3 = int(input('input your integer data: '))  # integer
+my_input4 = float(input('input your float data: '))  # float
+
+# ==============Print Function==========
+print(my_input)
+print(my_input2)
+print(my_input3)
+print(my_input4)
