2647edca63b3b9014706b54ea72cfb1d1e048faa
S M Farhan Qadir <113719435+thedecencycodified@users.noreply.github.com>
2022-09-20 15:17:18 +0500

    4.1 Numbers in python

---
+++ 4.1 Numbers in Python.py
@@ -0,0 +1,27 @@
+# str = strings(alphanumeric/alpha/special ch/numbers as text)
+
+# ==================== Numbers in python =====================
+# int = integers (numbers/numeric values) 99 44 5 0 1 1000 -55 -698 -102
+# float = numbers with decimal points 99.99 10.5 9.8 1.0 -3.3 -0.5 -100.22
+# complex =  5j, -5j
+
+# ==================== Integers =================
+# my_integer = 55
+# my_integer2 = -99
+# print(my_integer, my_integer2)
+# print(type(my_integer))
+# print(type(my_integer2))
+
+# ==================== floats =================
+# my_float = 55.55
+# my_float2 = -99.99
+# print(my_float, my_float2)
+# print(type(my_float))
+# print(type(my_float2))
+
+# ==================== Complex =================
+my_complex = 5j
+my_complex2 = -10j
+print(my_complex, my_complex2)
+print(type(my_complex))
+print(type(my_complex2))
