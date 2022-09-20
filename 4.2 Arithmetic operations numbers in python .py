760fbc038408390431f5000b8fa90b9e40c5f94f
S M Farhan Qadir <113719435+thedecencycodified@users.noreply.github.com>
2022-09-20 16:23:21 +0500

    4.2 Arithmetic operations p-1

---
+++ 4.2 Arithmetic operations numbers in python .py
@@ -0,0 +1,65 @@
+# ============== Arithmetic Operators =================
+# Arithmatic Operators + - * /  // ** %
+
+# ============== Addition ====================
+# int_addition = 5 + 8
+# print(int_addition)
+# float_addition = 5.5 + 9.99
+# print(float_addition)
+# print(99+88+55)
+# print(99.99+88.88+55.55)
+
+# ============== Subtraction ====================
+# int_Subtraction = 15 - 8
+# print(int_Subtraction)
+# float_Subtraction = 15.5 - 9.99
+# print(float_Subtraction)
+# print(1199-88-55)
+# print(1199.99-88.88-55.55)
+
+# ============== Multiplication ====================
+# int_multiplication = 5 * 3
+# print(int_multiplication)
+# float_multiplication = 5.6 * 9.89
+# print(float_multiplication)
+# print(9*8*7)
+# print(1.5*5.5*9.7)
+
+# ============== Division ====================
+# int_Division = 9 / 3
+# print(int_Division)
+# float_Division = 10.99 / 3.33
+# print(float_Division)
+# print(99/33)
+# print(44.55/33.56)
+
+# ============== Floor Division ====================
+# int_Division = 8//4
+# print(int_Division)
+# print(8//4)  # 8 = 4+4 =2
+# print(8//3)  # 8 = 3+3+2 = 2
+# print(20//6)  # 20 = 6+6+6+2 = 3
+# print(50//13)  # 50 = 13+13+13+11 = 3
+# float_Division = 8.8 // 3.33
+# print(float_Division)
+# print(55.55/55.54)
+# print(55.55//55.54)
+
+# ============== power ====================
+# int_power = 9 ** 3  # 9*9*9
+# print(int_power)
+# int_power1 = 5 ** 2  # 5*5
+# print(int_power1)
+# float_power = 10.99 ** 3.33
+# print(float_power)
+# print(99**2)
+# print(44.55**3.6)
+
+# =============== Modulus ===================
+modulus = 5 % 2  # 5=2*2=4+1 =>1       # remainder
+print(modulus)
+print(30 % 7)  # 30 = 7*4=28+2 => 2
+print(100 % 23)  # 100=23*4=92+8 =>8
+print(45 % 8)  # 45 = 8*5=40+5 =>5
+modulus2 = 7.66 % 2.88
+print(modulus2)
