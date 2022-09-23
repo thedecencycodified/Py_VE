6e4a144a95fb6b6a4da033bcf9b6e91618cd71f7
S M Farhan Qadir <113719435+thedecencycodified@users.noreply.github.com>
2022-09-23 08:06:30 +0500

    4.6 Casting, Booleans, Indexing & Math Module

---
+++ 4.6 Casting, Booleans, Indexing & Math Module .py
@@ -0,0 +1,54 @@
+# ================== Casting ================
+# name = str(input('Enter your name: '))
+# ID = int(input('Enter your ID: '))
+# percentage = float(input('Enter your percentage: '))
+# print(name)
+# print(ID)
+# print(percentage)
+
+# ================== Booleans ================
+# print(4 > 2)
+# print(5 == 8)
+# a = 'Hello World'
+# print(bool(a))
+# print(bool('Hello World'))
+# b = 555
+# print(bool(b))
+# c = None
+# print(bool(c))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool([]))
+# print(bool({}))
+
+# ================== Indexing ================
+#         0  1  2  3  4  5  6  7  8  9  10
+import math
+numbers = 1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9
+print(numbers)
+print(max(numbers))
+print(min(numbers))
+print(numbers.count(4))
+print(numbers.count(44))
+print(numbers.index(9))
+print(numbers.index(3))
+
+# ================== Math Module ================
+
+print(math.sqrt(64))
+print(math.ceil(99.99))
+print(math.floor(99.99))
+print(math.pow(5, 3))
+print(math.floor(math.pow(5, 3)))
+print(math.ceil(math.pow(5, 3)))
+print(math.log(9988))
+print(math.exp(155))
+print(math.cos(155))
+print(math.sin(155))
+print(math.tan(155))
+print(math.degrees(155))
+print(math.radians(155))
+print(math.cos(155.556))
