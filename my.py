a32066d28313ff384596fb646c47537629b6dde1
S M Farhan Qadir <113719435+thedecencycodified@users.noreply.github.com>
2022-10-29 01:37:27 +0500

    Topicwise Classification

--- my.py
+++
@@ -1,4 +0,0 @@
-Name = str(input("Enter your Name: "))
-percentage = int(input("Enter Your Percentage: "))
-if percentage >= 70:
-    print('{} you got {}% and placed in 1st Division'.format(Name, percentage))
